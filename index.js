08ca02e4f334e5043ff1659b4480e2c8aae01661
Subash Pradahan <suprowilde5@gmail.com>
2019-08-09 11:00:06 +0200

    fixed messages

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ const Sse =require('json-sse')
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
 
-const data = "hello world"
+const messages =['hello world']
 
-const sse = new Sse(data)
+const sse = new Sse(messages)
 
 const app = express()
 app.use(jsonParser)
-
 
 app.get('/stream', sse.init)
 
 app.post('/message', 
   (request, response) => {
     const {message} = request.body
+    messages.push(message)
+
+    sse.updateInit(messages)
     sse.send(message)
 
     response.send(message)
