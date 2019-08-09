5c5dd5adc66b3e318f066c2442b64321e09dd2a7
Subash Pradahan <suprowilde5@gmail.com>
2019-08-09 10:42:33 +0200

    Routing part post and get

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express')
+const Sse =require('json-sse')
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
+
+const data = "hello world"
+
+const sse = new Sse(data)
 
 const app = express()
+app.use(jsonParser)
+
+
+app.get('/stream', sse.init)
+
+app.post('/message', 
+  (request, response) => {
+    const {message} = request.body
+    sse.send(message)
+
+    response.send(message)
+  }
+)
+
 const port = process.env.PORT || 5000
 
 app.listen(
